bd60bc1696a8950c214d4ed289c1688fe5bb2bf4
Ilham D. Sofyan <ilhamdsofyan@gmail.com>
2025-12-30 09:36:14 +0700

    Perbaikan filepath image profile

--- application/views/layouts/main.php
+++ application/views/layouts/main.php
@@ -105,7 +105,7 @@
                         </h6>
                         <!-- Example Message 1  -->
                         <a class="dropdown-item dropdown-notifications-item" href="#!">
-                            <img class="dropdown-notifications-item-img" src="assets/img/illustrations/profiles/profile-2.png">
+                            <img class="dropdown-notifications-item-img" src="<?= base_url('web/assets/img/illustrations/profiles/profile-2.png') ?>">
                             <div class="dropdown-notifications-item-content">
                                 <div class="dropdown-notifications-item-content-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
                                 <div class="dropdown-notifications-item-content-details">Thomas Wilcox · 58m</div>
